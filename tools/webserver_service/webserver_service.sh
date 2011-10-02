fb54368aecf5c3c3afbe81c65d7178be10ed8502
aurel <aurel@ubuntu.ubuntu-domain>
2011-10-03 01:07:57 +0200

    deploy

--- tools/webserver_service/webserver_service.sh
+++ tools/webserver_service/webserver_service.sh
@@ -4,4 +4,4 @@ APPLI_DIR=/home/node/moa
 
 current_pid=`ps x | grep \'node web-server.js\' | grep -v grep | cut -d\' \' -f 1`
 [ $current_pid != "" ] && kill -9 $current_pid
-cd $APPLI_DIR && node web-server.js 2> &1 > /var/log/moa.log
+cd $APPLI_DIR && node web-server.js 2>&1 > /var/log/moa.log
