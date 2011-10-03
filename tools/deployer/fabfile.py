7367289d40a63917393cf289344c796ee0548c8c
Aurelien Gasser <aurelien.gasser@gmail.com>
2011-10-03 21:17:39 +0200

    deployer

--- tools/deployer/fabfile.py
+++ tools/deployer/fabfile.py
@@ -1,34 +1,31 @@
 from fabric.api import *
 
-application_dir        = '/home/moa/'
-repo                   = 'https://github.com/pouyajoon/moa'
+app                    = 'moa'
+app_dir                = '/home/' + app + '/'
+repo                   = 'https://github.com/pouyajoon/' + app
 code_fetched_from_repo = False
 
 
 env.hosts = ['root@91.121.223.149']
 
-def deploy(version = ''):
+def deploy(version = 'HEAD'):
 	deploy_config(version)
 	deploy_tools(version)
 	deploy_appli(version)
 
-def get_code_from_repo(version = ''):
+def get_code_from_repo(version = 'HEAD'):
 	global code_fetched_from_repo
 	if (code_fetched_from_repo == False):
-		run('rm -rf /tmp/moa')
-		run('mkdir /tmp/moa');
-		run('cd /tmp/moa && git init && git remote add origin ' + repo + ' && git fetch origin ' + version + ' && git reset --hard FETCH_HEAD');
+		run('rm -rf /tmp/' + app)
+		run('cd /tmp && git clone -n ' + repo + ' && cd ' + app + ' && git archive ' + version + ' | tar x');
 		code_fetched_from_repo = True
 
 # APPLI
 
-def deploy_appli(version = ''):
+def deploy_appli(version = 'HEAD'):
 	get_code_from_repo()
-	run('rm -rf ' + application_dir + '/server')
-	run('rm -rf ' + application_dir + '/client')
-	run('mkdir -p ' + application_dir)
-	run('cp -R ' + '/tmp/moa/server ' + application_dir + 'server');
-	run('cp -R ' + '/tmp/moa/client ' + application_dir + 'client');
+	run('rsync -a ' + '/tmp/' + app + '/server/ ' + app_dir + 'server/');
+	run('rsync -a ' + '/tmp/' + app + '/client/ ' + app_dir + 'client/');
 	after_deploy_appli()
 
 def after_deploy_appli():
@@ -36,18 +33,16 @@ def after_deploy_appli():
 
 def restart_node_appli():
 	# nohup + pipe redirections + & => required to make it nonblocking
-	run('nohup ' + application_dir + 'tools/start_webserver/start_webserver.sh >& /dev/null  &', False, False)
+	run('nohup ' + app_dir + 'tools/start_webserver/start_webserver.sh >& /dev/null  &', False, False)
 
 # TOOLS
 
-def deploy_tools(version = ''):
+def deploy_tools(version = 'HEAD'):
 	get_code_from_repo(version)
-	run('rm -rf ' + application_dir + '/tools')
-	run('mkdir -p ' + application_dir)
-	run('cp -R ' + '/tmp/moa/tools ' + application_dir + 'tools');
+	run('rsync -a ' + '/tmp/' + app + '/tools/ ' + app_dir + 'tools/');
 
 # CONFIG
 
-def deploy_config(version = ''):
+def deploy_config(version = 'HEAD'):
 	get_code_from_repo(version)
 
